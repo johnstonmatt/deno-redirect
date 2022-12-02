0362839dab42db31a91ab08f6a00ae4a5ccd30b5
Matt Johnston <m@ttjohnston.com>
2022-12-02 19:30:56 -0400

    redirect to env.TO or this repo

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
 
-const Location = Deno.env.get("LOCATION") || "World";
+const Location = Deno.env.get("TO") || 'https://github.com/johnstonmatt/deno-redirect';
 const PERMANENT_REDIRECT = 301;
 
 serve(
