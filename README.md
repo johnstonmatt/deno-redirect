c5d17c4021027d615285d892ee15b1404ec8c8d3
Matt Johnston <m@ttjohnston.com>
2022-12-02 19:27:33 -0400

    one and done?

---
+++ README.md
@@ -0,0 +1,3 @@
+# deno-redirect
+
+A simple redirect edge function designed for [deno deploy](https://deno.com)
--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const Location = Deno.env.get("LOCATION") || "World";
+const PERMANENT_REDIRECT = 301;
+
+serve(
+  (_: Request) =>
+    new Response("Hello World", {
+      status: PERMANENT_REDIRECT,
+      headers: { Location },
+    })
+);
